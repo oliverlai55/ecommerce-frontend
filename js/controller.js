0d413b480d386aeb4ed4eb5c2544467dfaceb702
Wei lun Lai <oliverlai55@gmail.com>
2015-12-21 15:50:42 -0500

    manipulating json object

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,20 @@
-var ecommerceApp = angular.module('ecommerce'){
-	
-	ecommerceApp.controller('ecommerce-controller', function($scope, $http){
+var ecommerceApp = angular.module('ecommerce', []);
+
+	ecommerceApp.controller('ecommerce-controller', function ($scope, $http){
 		$scope.ninja = "DigitalCrafts students";
 
+		$scope.weatherJSON = [];
+		$http.get('http://localhost:3000').then(function (theData){
+			//then tells it to first get data before $scope runs it
+			$scope.weatherJSON = theData.data;
+
+			var calvinTemp = theData.data.main.temp;
+			var FahrenTempRaw = (calvinTemp - 273.15)*1.8 +32;
+			$scope.FahrenTemp = FahrenTempRaw.toFixed(0);
+			console.log(FahrenTempRaw.toFixed(0));
+
+			console.log(theData.data);
+			console.log(theData.data.sys.country);
+		})
 
 	});
