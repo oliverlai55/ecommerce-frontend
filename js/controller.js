ca3ea6f2121a960c88f67f8a60d8611455828d37
Wei lun Lai <oliverlai55@gmail.com>
2015-12-22 10:47:13 -0500

    html changes

--- js/controller.js
+++ js/controller.js
@@ -3,19 +3,19 @@ var ecommerceApp = angular.module('ecommerce', []);
 ecommerceApp.controller('ecommerce-controller', function ($scope, $http){
 	$scope.ninja = "DigitalCrafts students";
 
-	$scope.weatherJSON = [];
-	$http.get('http://localhost:3000').then(function (theData){
-			//then tells it to first get data before $scope runs it
-			$scope.weatherJSON = theData.data;
+	// $scope.weatherJSON = [];
+	// $http.get('http://localhost:3000').then(function (theData){
+	// 		//then tells it to first get data before $scope runs it
+	// 		$scope.weatherJSON = theData.data;
 
-			var calvinTemp = theData.data.main.temp;
-			var FahrenTempRaw = (calvinTemp - 273.15)*1.8 +32;
-			$scope.FahrenTemp = FahrenTempRaw.toFixed(0);
-			console.log(FahrenTempRaw.toFixed(0));
+	// 		var calvinTemp = theData.data.main.temp;
+	// 		var FahrenTempRaw = (calvinTemp - 273.15)*1.8 +32;
+	// 		$scope.FahrenTemp = FahrenTempRaw.toFixed(0);
+	// 		console.log(FahrenTempRaw.toFixed(0));
 
-			console.log(theData.data);
-			console.log(theData.data.sys.country);
-		})
+	// 		console.log(theData.data);
+	// 		console.log(theData.data.sys.country);
+	// 	})
 
 
 	//This will fire on ng-submit. We want to make a post request to the server
